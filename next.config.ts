50ff912492a0d26b6e1245de15f136738ac74399
torgomyan01 <and.torgomyan01@gmail.com>
2026-02-03 20:23:57 +0400

    Fix bugs

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-import path from "path";
+import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Отключаем проверку ESLint во время production build
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Отключаем проверку TypeScript во время production build (опционально)
     ignoreBuildErrors: false,
@@ -17,43 +13,43 @@ const nextConfig = {
   env: {
     // NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
   },
-  transpilePackages: ["@mep-agency/next-iubenda"],
+  transpilePackages: ['@mep-agency/next-iubenda'],
   reactStrictMode: true,
   sassOptions: {
     additionalData: ``,
-    includePaths: [path.join(__dirname, "src/access/css")],
+    includePaths: [path.join(__dirname, 'src/access/css')],
   },
   images: {
     remotePatterns: [
       {
-        hostname: "**",
+        hostname: '**',
       },
     ],
   },
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/(.*)',
         headers: [
           {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains; preload", // Հատուկ պարամետրեր
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload', // Հատուկ պարամետրեր
           },
           {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
           },
           {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
           },
           {
-            key: "Referrer-Policy",
-            value: "strict-origin",
+            key: 'Referrer-Policy',
+            value: 'strict-origin',
           },
           {
-            key: "Permissions-Policy",
-            value: "geolocation=(), camera=(), microphone=()",
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), camera=(), microphone=()',
           },
         ],
       },
